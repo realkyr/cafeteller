9300556e08c73f04a2017dde415a1cc4b46da258
realkyr <60070075@kmitl.ac.th>
2021-01-25 15:57:42 +0700

    ✨ add loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Router from 'next/router'
+import NProgress from 'nprogress' // nprogress module
+import 'nprogress/nprogress.css'
 import 'antd/dist/antd.css'
 import '../styles/globals.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp ({ Component, pageProps }) {
   return (
