fe098504fba134f9b6535458d8f23bb7156a5fb2
realkyr <phuree.kanusont@gmail.com>
2024-08-07 09:55:17 +0700

    ci(warmup): add warmup handler for dev

--- next.config.js
+++ next.config.js
@@ -2,13 +2,5 @@ module.exports = {
   distDir: 'build',
   eslint: {
     ignoreDuringBuilds: true
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.json$/,
-      type: 'json'
-    })
-
-    return config
   }
 }
